6fb6b9cdc43fcf58b7731609300b95f95b3f3fbc
Wellington Mota <impeto.wellington@gmail.com>
2024-11-18 17:09:34 +0000

    endpoint 3

--- pages/api/submitForm.js
+++ pages/api/submitForm.js
@@ -1,21 +1,28 @@
 // api/submitForm.js
 
-// Importa o array de submissões
-import { submissions } from './getSumissions';
+import { submissions } from './getSubmissions';
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email, message } = req.body;
 
+    // Verifica se os campos estão preenchidos
+    if (!name || !email || !message) {
+      return res.status(400).json({
+        error: 'Todos os campos são obrigatórios.',
+      });
+    }
+
     // Adiciona os dados ao array de submissões
     submissions.push({ name, email, message, date: new Date() });
 
+    // Envia a resposta
     res.status(200).json({
-      message: "Formulário recebido com sucesso!",
+      message: 'Formulário recebido com sucesso!',
       data: { name, email, message },
     });
   } else {
-    res.setHeader("Allow", ["POST"]);
+    res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
